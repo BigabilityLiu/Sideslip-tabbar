89b362890bfdbcc2878513b96457cb1941cb2b4b
刘大本事 <1130471993@qq.com>
2016-01-21 14:31:04 +0800

    修改完善搜索功能

--- Demo-2搜索/Sideslip&TabbarLikeQQDemo/Sideslip&TabbarLikeQQDemo/SecondViewController.swift
+++ Demo-2搜索/Sideslip&TabbarLikeQQDemo/Sideslip&TabbarLikeQQDemo/SecondViewController.swift
@@ -11,24 +11,21 @@ import UIKit
 class SecondViewController: UITableViewController {
 
     let items:NSMutableArray = [["name":"大本事","code":"10001"],["name":"二本事","code":"10010"],["name":"三本事","code":"10100"],["name":"四本事","code":"11000"],["name":"小本事","code":"11111"],["name":"没本事","code":"12301"],["name":"没文化","code":"12303"],["name":"迪迦","code":"20223"],["name":"奥特","code":"20339"],["name":"钢铁侠","code":"34303"],["name":"雷神","code":"30002"],["name":"绿巨人","code":"30434"],["name":"美国队长","code":"30043"],["name":"黑寡妇","code":"30234"]]
+    var resultsArray = NSMutableArray()
     var searchController: UISearchController!
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         initSearchController()
+        tableView.tableFooterView = UIView()
     }
     func initSearchController(){
         //1
-        
-        let searchResultsController = ResultsTableViewController()
-        searchResultsController.originalArray = items
-        searchController = UISearchController(searchResultsController: searchResultsController)
-        searchController.searchResultsUpdater = searchResultsController
-        
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
         //2
         searchController.hidesNavigationBarDuringPresentation = true
-        searchController.dimsBackgroundDuringPresentation = true
+        searchController.dimsBackgroundDuringPresentation = false
         //3
         let searchBar = searchController.searchBar
         self.tableView.tableHeaderView = searchBar
@@ -44,14 +41,36 @@ class SecondViewController: UITableViewController {
         return 1
     }
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return self.searchController.active ? resultsArray.count :items.count
     }
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-        
-        cell.textLabel?.text = items[indexPath.row]["name"] as? String
-        cell.detailTextLabel?.text = items[indexPath.row]["code"] as? String
+        var data:NSArray!
+        if self.searchController.active{
+            data = resultsArray
+        }else{
+            data = items
+        }
+        cell.textLabel?.text = data[indexPath.row]["name"] as? String
+        cell.detailTextLabel?.text = data[indexPath.row]["code"] as? String
         
         return cell
     }
+}
+extension SecondViewController: UISearchResultsUpdating{
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        let target = searchBar.text
+        var filteredArray = NSMutableArray()
+        //名字包含输入内容或者工号以输入内容开头
+        let predicate = NSPredicate(format: "name CONTAINS %@ || code BEGINSWITH %@",target!,target!)
+        let results = items.filteredArrayUsingPredicate(predicate)
+        for item in results{
+            filteredArray.addObject(item)
+        }
+        resultsArray = filteredArray
+        filteredArray = []
+        self.tableView.reloadData()
+    }
+
 }
